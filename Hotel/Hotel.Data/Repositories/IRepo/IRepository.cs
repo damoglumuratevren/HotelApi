b80631be9b48ffc93fbe79ec13ae237a4f2c5921
damoglumuratevren <60298354+damoglumuratevren@users.noreply.github.com>
2022-07-06 18:27:31 +0300

    repolar eklendi

---
+++ Hotel/Hotel.Data/Repositories/IRepo/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Data.Repositories.IRepo
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<ICollection<T>> GetAll(Expression<Func<T, bool>> filter, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperites = null);
+
+        Task<T> Get(string id);
+
+        Task<T> GetFirstOrDefault(Expression<Func<T, bool>> filter = null, string includeProperites = null);
+
+
+        Task<bool> Add(T entity);
+
+        Task<bool> Update(T entity);
+
+        bool Delete(T entity);
+
+        bool UpdateIsActive(string id);
+    }
+}
---
+++ Hotel/Hotel.Data/Repositories/Repo/Repository.cs
@@ -0,0 +1,50 @@
+﻿using Hotel.Data.Repositories.IRepo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Data.Repositories.Repo
+{
+    public class Repository<T> : IRepository<T> where T : class MyClass
+    {
+        public Task<bool> Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Get(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<T>> GetAll(Expression<Func<T, bool>> filter, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperites = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetFirstOrDefault(Expression<Func<T, bool>> filter = null, string includeProperites = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateIsActive(string id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    {
+    }
+}
