45976a3828ff69ff63d976e9bb60a46d47593df8
damoglumuratevren <60298354+damoglumuratevren@users.noreply.github.com>
2022-07-14 10:18:06 +0300

    iii

--- Hotel/Hotel.Api/Controllers/PersonelTypeController.cs
+++ Hotel/Hotel.Api/Controllers/PersonelTypeController.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
 using Hotel.Data.Services.IService.IPersonelSv;
 using Hotel.Model.Dtos.PersonelDt;
+using Hotel.Model.Models.PersonelMd;
 using Microsoft.AspNetCore.Mvc;
+using NUlid;
 
 namespace Hotel.Api.Controllers
 {
@@ -25,5 +27,28 @@ namespace Hotel.Api.Controllers
             var personelTypeDto = _mapper.Map<IEnumerable<PersonelTypeDto>>(personelType);
             return Ok(personelTypeDto); 
         }
+
+        [HttpGet("{Id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            var personeltype= await _personelTypeService.Get(id);
+            var personelTypeDto = _mapper.Map<PersonelTypeDto>(personeltype);
+            return Ok(personelTypeDto);
+        }
+        [HttpPost]
+        public async Task<IActionResult> PersonelTypeAdd(PersonelTypeDto personelTypeDto)
+        {
+            //personelTypeDto.Id = Ulid.NewUlid().ToString();
+
+            var personelType = _mapper.Map<PersonelType>(personelTypeDto);
+            personelType.Id = Ulid.NewUlid().ToString();
+            personelType.CreatedId = "01ASB2XFCZJY7WHZ2FNRTMQJCT";
+            personelType.CreatedDtm=DateTime.Now;
+            var newPersonelType = await _personelTypeService.Add(personelType);
+            return Created(String.Empty, _mapper.Map<PersonelTypeDto>(newPersonelType));
+           // return Created(String.Empty,newPersonelType);
+
+        }
+
     }
 }
